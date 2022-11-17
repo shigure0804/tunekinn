a9ec1cf9acee1b9b9c80ef258e28ff9b0d239369
shigure0804 <88415281+shigure0804@users.noreply.github.com>
2022-11-17 11:19:54 +0900

    タイトルからシーンにいくきのうを追加

---
+++ Assets/Sclipt/switching.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class switching : MonoBehaviour
+{
+    public void OnclicksoioBottan()
+    {
+        SceneManager.LoadScene("play1");
+    }
+    
+    
+    // Start is called before the first frame update
+   
+}
